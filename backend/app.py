7c33466d51921c885c20bb34d2cf22eace1943ff
sistemasuni-cesu <palmaunicesumar@gmail.com>
2025-08-21 12:05:38 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -34,3 +34,8 @@ def upload():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render define a porta
     app.run(host="0.0.0.0", port=port)
+
+from flask_cors import CORS
+
+CORS(app)  # habilita CORS para todas as rotas
+
